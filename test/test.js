6059930331bc91ffb8b85304d2f728796141eca2
Ger Hobbelt <ger@hobbelt.com>
2021-02-12 14:59:57 +0100

    updated packages

--- test/test.js
+++ test/test.js
@@ -39,7 +39,7 @@ function generate(fixturePath, md, env) {
 
 
 describe('markdown-it-abbr', function () {
-  let md = markdown_it({ linkify: true })
+  const md = markdownit({ linkify: true })
               .use(plugin);
 
   generate(path.join(__dirname, 'fixtures/critic.txt'), md);
